fb7216762fe1bf7043fc666d9b57062bd6231787
Carlos Rueda Morales <carlos.rueda@deimos-space.com>
2016-11-10 17:00:26 +0100

    Moved something to somewhere... goodnight...

---
+++ www/js/const.js
@@ -0,0 +1,12 @@
+/*
+var urlLogin = "http://view.kyroslbs.com:3000/api/login"
+var urlArchiveNotification = "http://view.kyroslbs.com/api/notification/archive";
+var urlGetNotifications = "http://view.kyroslbs.com/api/notification"
+var urlTreeDevices = "http://view.kyroslbs.com/api/monitorTree";
+*/
+
+var urlLogin = "http://localhost:3000/api/login"
+var urlArchiveNotification = "http://localhost:3000/api/notification/archive";
+var urlGetNotifications = "http://localhost:3000/api/notification"
+var urlTreeDevices = "http://localhost:3000/api/monitorTree";
+var urlSaveToken = "http://localhost:3000/api/notification/setToken";
